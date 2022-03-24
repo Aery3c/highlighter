c84bdf5c4501deb03d6fac6224a603e30e2714bd
Aery <294725131@qq.com>
2022-03-24 18:34:03 +0800

    feat: 使用webpack-dev-server开发

--- dev.js
+++ dev.js
@@ -3,41 +3,50 @@
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpackDevServer = require('webpack-dev-server');
 const path = require('path');
 
 const config = {
-  entry: path.resolve(__dirname, 'src/highlighter'),
+  entry: path.resolve(__dirname, 'src/index.js'),
+  devtool: 'eval-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'highlighter.bundle.js'
+    filename: 'highlighter.dev.js',
+    publicPath: '/',
+    library: {
+      name: 'Highlighter',
+      type: 'umd',
+      export: 'default',
+    },
+    globalObject: 'this'
+  },
+  devServer: {
+    hot: true,
+    compress: true,
+    open: true,
+    port: 3000,
   },
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'test',
       filename: 'demos/test.html',
-      template: path.resolve(__dirname, 'template/test.html')
-    })
+      template: path.resolve(__dirname, 'template/test.html'),
+      publicPath: '../',
+      scriptLoading: 'blocking'
+    }),
+    new ESLintPlugin()
   ]
 };
 
 const compiler = webpack(config);
-compiler.run((err, stats) => {
-  if (err) {
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
-    }
-    return;
-  }
+const devServerOptions = { ...config.devServer };
+const server = new webpackDevServer(devServerOptions, compiler);
 
-  const info = stats.toJson();
+const runServer = async () => {
+  console.log('Starting server...');
+  await server.start();
+};
 
-  if (stats.hasErrors()) {
-    console.error(info.errors);
-  }
-
-  if (stats.hasWarnings()) {
-    console.warn(info.warnings);
-  }
-});
+runServer();
