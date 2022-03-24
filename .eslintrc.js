48c5a337e82896a9be649e62bbd1ae30b6ed4efc
Aery <294725131@qq.com>
2022-03-24 18:33:19 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es2021": true,
-    "node": true
+    "node": true,
+    // "amd": true,
   },
   "extends": "eslint:recommended",
   "parserOptions": {
