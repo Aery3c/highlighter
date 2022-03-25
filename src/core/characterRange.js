b6136dacce7852131c240603b05d41629874f604
Aery <294725131@qq.com>
2022-03-25 17:34:49 +0800

    Update characterRange.js

--- src/core/characterRange.js
+++ src/core/characterRange.js
@@ -1,18 +1,26 @@
-/**
- *
- * @param {Range} range
- * @param {HTMLElement} containerElement
- * @return {CharacterRange}
- */
-// eslint-disable-next-line no-unused-vars
-function rangeToCharacterRange(range, containerElement) {
-  // todo
-  // const bookMark = range.getBookMark(containerElement);
-}
+'use strict'
 
 export default class CharacterRange {
+  /**
+   *
+   * @param {number} start
+   * @param {number} end
+   */
   constructor(start, end) {
     this.start = start;
     this.end = end;
+  }
+
+  /**
+   *
+   * @param {Range} range
+   * @param {HTMLElement} containerElement
+   * @return {CharacterRange}
+   */
+
+  static fromRange (range, containerElement) {
+    /** @type {BookMark} */
+    const bookMark = range.getBookMark(containerElement);
+    return new CharacterRange(bookMark.start, bookMark.end)
   }
 }
