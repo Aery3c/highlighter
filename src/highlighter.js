59353f52df1e49380ad1f5c94629b59b31c2ea2e
Aery <294725131@qq.com>
2022-03-25 17:35:23 +0800

    Update highlighter.js

--- src/highlighter.js
+++ src/highlighter.js
@@ -1,16 +1,18 @@
 'use strict'
 
 import { createOptions } from './utils';
+import { getContainerElement } from './dom';
+import CharacterRange from './core/characterRange';
 
-const optionPropNames = ['className', 'tagName', 'containerElementId'];
+const highligterPropsNames = ['className', 'tagName'];
 
 export default class Highlighter {
-  constructor(options) {
+  constructor (options = {}) {
     options = createOptions(options, {
       className: 'highlight',
       tagName: 'span'
     });
-    optionPropNames.forEach(propName => {
+    highligterPropsNames.forEach(propName => {
       if (Object.prototype.hasOwnProperty.call(options, propName)) {
         this[propName] = options[propName];
       }
@@ -19,11 +21,55 @@ export default class Highlighter {
 
   /**
    *
-   * @param {Range} range
-   * @private
+   * @param {{}} [options]
    */
-  _apply() {
+  highlightToSelection (options = {}) {
+    options = createOptions(options, {
+      containerElementId: null,
+    });
 
+    const containerElementId = options.containerElementId;
+    const containerElement = getContainerElement(containerElementId);
+    const selection = options.selection || window.getSelection();
+    const serialized = serializeSelection(selection, containerElement);
+
+    const characterRanges = [];
+    serialized.forEach(({ characterRange } )=> {
+      characterRanges.push(characterRange);
+    })
+
+    return this.highlightToCharacterRanges(characterRanges, containerElementId);
   }
 
+  /**
+   *
+   * @param {CharacterRange[]} characterRanges
+   * @param {string} containerElementId
+   * @return {Highlight[]}
+   */
+  highlightToCharacterRanges (characterRanges, containerElementId) {
+    characterRanges.forEach(characterRange => {
+      // todo
+    });
+  }
+
+}
+
+/**
+ *
+ * @param {Selection} selection
+ * @param {HTMLElement} containerElement
+ * @return {{ characterRange: CharacterRange, isBackward: boolean }[]}
+ */
+export function serializeSelection (selection, containerElement) {
+  const selInfos = [];
+  const ranges = selection.getAllRange();
+  ranges.forEach(range => {
+    selInfos.push({
+      characterRange: CharacterRange.fromRange(range, containerElement),
+      isBackward: selection.isBackward()
+    })
+  });
+
+  return selInfos;
 }
