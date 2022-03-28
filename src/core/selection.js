54b666edc9019ab9f72143d8373928dd502d0525
Aery <294725131@qq.com>
2022-03-28 17:01:45 +0800

    Update selection.js

--- src/core/selection.js
+++ src/core/selection.js
@@ -1,29 +1,18 @@
 'use strict'
-
-export default {
-  /**
-   *
-   * @return {Range[]}
-   */
-  getAllRange () {
-    /** @this Selection */
-    const ranges = [];
-    for (let i = 0; i < this.rangeCount; ++i) {
-      ranges.push(this.getRangeAt(i));
-    }
-    return ranges;
-  },
-  /**
-   *
-   * @return {boolean}
-   */
-  isBackward () {
-    /** @this Selection */
-    let isBackward = false, anchorNode = this.anchorNode, focusNode = this.focusNode;
-    if (!this.isCollapsed && anchorNode) {
-      isBackward = !!(anchorNode.compareDocumentPosition(focusNode) & 2);
-    }
-
-    return isBackward;
+/** @this Selection */
+Selection.prototype.getAllRange = function () {
+  const ranges = [];
+  for (let i = 0; i < this.rangeCount; ++i) {
+    ranges.push(this.getRangeAt(i));
   }
+  return ranges;
+}
+
+Selection.prototype.isBackward = function () {
+  let isBackward = false, anchorNode = this.anchorNode, focusNode = this.focusNode;
+  if (!this.isCollapsed && anchorNode) {
+    isBackward = !!(anchorNode.compareDocumentPosition(focusNode) & 2);
+  }
+
+  return isBackward;
 }
