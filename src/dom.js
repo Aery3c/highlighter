6c9f3adeb2076ce420442dc0d7857f11dc6a7cc3
Aery <294725131@qq.com>
2022-03-28 17:00:27 +0800

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -8,3 +8,27 @@
 export function getContainerElement (id) {
   return id ? document.getElementById(id) : document.body;
 }
+
+/**
+ *
+ * @param {Node} node
+ * @return {boolean}
+ */
+export function isCharacterDataNode (node) {
+  const t = node.nodeType;
+  return t === Node.TEXT_NODE || t === Node.COMMENT_NODE || t === Node.CDATA_SECTION_NODE;
+}
+
+/**
+ *
+ * @param {Node} node
+ * @return {number}
+ */
+export function getNodeIndex (node) {
+  let index = 0;
+  while ((node = node.previousSibling)) {
+    index++;
+  }
+
+  return index;
+}
