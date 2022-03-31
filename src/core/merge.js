6e7ec6e53efc796f7c01e7f00ccd15060a089f55
Aery <294725131@qq.com>
2022-03-31 17:49:34 +0800

    Create merge.js

---
+++ src/core/merge.js
@@ -0,0 +1,39 @@
+'use strict';
+import { removeNode } from '../dom';
+
+export default class Merge {
+  /**
+   *
+   * @param {Node} node
+   */
+  constructor(node) {
+    this.firstTextNode = node.nodeType === Node.ELEMENT_NODE ? node.firstChild : node;
+    this.textNodes = [this.firstTextNode];
+  }
+
+  start () {
+    const textParts = [];
+    this.textNodes.forEach((textNode, index) => {
+      const parentNode = textNode.parentNode;
+      if (index > 0) {
+        removeNode(textNode);
+        if (!parentNode.hasChildNodes()) {
+          removeNode(parentNode);
+        }
+      }
+      textParts.push(textNode.data);
+    });
+
+    this.firstTextNode.data = textParts.join('');
+    return this.firstTextNode.data;
+  }
+
+  getNodeLength () {
+    let len;
+    this.textNodes.forEach(textNode => {
+      len += textNode.length;
+    });
+    return len;
+  }
+
+}
