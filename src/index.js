073e3f2f5ae798450a2da485bd51accc936d96be
Aery <294725131@qq.com>
2022-04-06 18:26:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './core/selection';
 import Highlighter from './highlighter';
 import Highlight from './core/highlight';
 import CharacterRange from './core/characterRange';
+import Applier from './core/applier';
 import * as utils from './utils';
 import * as dom from './dom';
 
@@ -10,5 +11,6 @@ Highlighter.utils = utils;
 Highlighter.dom = dom;
 Highlighter.Highlight = Highlight;
 Highlighter.CharacterRange = CharacterRange;
+Highlighter.Applier = Applier;
 
 export default Highlighter
