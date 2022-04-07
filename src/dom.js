ea3571fbbf1054aba56c11ae0b366ef5cb73058a
Aery <294725131@qq.com>
2022-04-07 18:33:16 +0800

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -150,7 +150,7 @@ export function removeNode (node) {
 export function moveChildren (node, parentNode, removeSelf) {
   let child, children = [], index = getNodeIndex(node);
   while ((child = node.firstChild)) {
-    parentNode.insertBefore(child, parentNode.childNodes[index++]);
+    moveChild(child, parentNode, index++);
     children.push(child);
   }
 
@@ -159,4 +159,96 @@ export function moveChildren (node, parentNode, removeSelf) {
   }
 
   return children;
+}
+
+function moveChild (node, parentNode, index) {
+  // For convenience, allow newIndex to be -1 to mean "insert at the end".
+  if (index === -1) {
+    index = parentNode.childNodes.length;
+  }
+
+  // Now actually move the node.
+  if (parentNode.childNodes.length === index) {
+    parentNode.appendChild(node);
+  } else {
+    parentNode.insertBefore(node, parentNode.childNodes[index]);
+  }
+}
+
+/**
+ *
+ * @param {Node} ancestor
+ * @param {Node} descendant
+ * @param {number} descendantOffset
+ */
+
+export function splitNodeAt(ancestor, descendant, descendantOffset) {
+  let newNode, parentNode;
+  const splitAtStart = (descendantOffset === 0);
+
+  if (isAncestorOf(descendant, ancestor)) {
+    return ancestor;
+  }
+
+  if (isCharacterDataNode(descendant)) {
+    let descendantIndex = getNodeIndex(descendant);
+    if (descendantOffset === 0) {
+      descendantOffset = descendantIndex;
+    } else if (descendantOffset === descendant.length) {
+      descendantOffset = descendantIndex + 1;
+    }
+    descendant = descendant.parentNode;
+  }
+
+  if (isSplitPoint(descendant, descendantOffset)) {
+    // descendantNode is now guaranteed not to be a text or other character node
+    newNode = descendant.cloneNode(false);
+    parentNode = descendant.parentNode;
+    if (newNode.id) {
+      newNode.removeAttribute("id");
+    }
+    let child, newChildIndex = 0;
+
+    while ( (child = descendant.childNodes[descendantOffset]) ) {
+      moveChild(child, newNode, newChildIndex++);
+    }
+    moveChild(newNode, parentNode, getNodeIndex(descendant) + 1);
+    return (descendant === ancestor) ? newNode : splitNodeAt(ancestor, parentNode, getNodeIndex(newNode));
+
+  } else if (ancestor !== descendant) {
+    newNode = descendant.parentNode;
+
+    // Work out a new split point in the parent node
+    let newNodeIndex = getNodeIndex(descendant);
+
+    if (!splitAtStart) {
+      newNodeIndex++;
+    }
+    return splitNodeAt(ancestor, newNode, newNodeIndex);
+  }
+  return ancestor;
+}
+
+function isSplitPoint(node, offset) {
+  if (isCharacterDataNode(node)) {
+    if (offset === 0) {
+      return !!node.previousSibling;
+    } else if (offset === node.length) {
+      return !!node.nextSibling;
+    } else {
+      return true;
+    }
+  }
+
+  return offset > 0 && offset < node.childNodes.length;
+}
+
+/**
+ *
+ * @param {Node} node
+ * @return {boolean}
+ */
+export function isEmptyElement (node) {
+  const childCount = node.childNodes.length;
+  return node.nodeType === Node.ELEMENT_NODE && (childCount === 0) || (childCount === 1 && isEmptyElement(node.firstChild));
 }
