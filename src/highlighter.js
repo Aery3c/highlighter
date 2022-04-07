a8c107281bb5d7e4a809b294355d7b2f206b038b
Aery3c <294725131@qq.com>
2022-04-07 22:54:28 +0800

    refactor: highlighter.js

--- src/highlighter.js
+++ src/highlighter.js
@@ -1,7 +1,23 @@
 'use strict'
+import Applier from './core/applier';
 
 export default class Highlighter {
-  constructor (options = {}) {
-
+  constructor(options = {}) {
+    this.highlights = [];
+    this._applier = new Applier(options);
   }
+
+  /**
+   * @param {any} [thing]
+   */
+  paint (thing) {
+    let range;
+    if (thing instanceof Selection) {
+
+    } else if (thing instanceof Range) {
+
+    }
+  }
+
 }
+
