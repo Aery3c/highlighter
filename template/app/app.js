f2fa13949f108231363e3fe4b0604075b16535f0
Aery <294725131@qq.com>
2022-04-19 18:38:43 +0800

    Update app.js

--- template/app/app.js
+++ template/app/app.js
@@ -1,1 +1,14 @@
 'use strict'
+
+import { createPopper } from '@popperjs/core';
+import Highlighter from '@/index';
+import './app.scss';
+
+(function() {
+
+  document.addEventListener('mouseup', () => {
+    const range = window.getSelection().getRangeAt(0);
+
+  }, false);
+
+})();
