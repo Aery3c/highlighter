9632d7d741151ed7f40c341dbe5f9ebc0b2bc859
Aery <294725131@qq.com>
2022-04-28 18:14:50 +0800

    Update app.js

--- template/app/app.js
+++ template/app/app.js
@@ -1,5 +1,23 @@
 'use strict'
 
-import '@/index';
-import './contextMenu';
+import Highlighter from "@/index";
+import createContextMenu from './contextMenu';
 import './app.scss';
+
+const higlighter = new Highlighter({});
+
+createContextMenu(
+  '.book',
+  [{
+    name: 'highlighter',
+    click: () => {
+      console.log('run this');
+      const highlights = higlighter.highlightSelection();
+      console.log(highlights);
+    }
+  }]
+);
+
+document.addEventListener('selectstart', function () {
+  console.log('selectstart');
+});
