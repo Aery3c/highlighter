84e06764791e83d19306b9ffcd3769be3626a51a
Aery <294725131@qq.com>
2022-04-28 18:14:09 +0800

    Update index.js

--- template/app/contextMenu/index.js
+++ template/app/contextMenu/index.js
@@ -1,59 +1,98 @@
 'use strict'
 
+import '@/core/selection';
 import { createPopper } from '@popperjs/core';
-import { toggleClass } from '@/dom';
+import { toggleClass, addClass } from '@/dom';
 import './styles.scss';
 
-const pos = 0; // init position
-
-const bookContent = getElement('.book_content');
-const menu = getElement('.context_menu');
-
-const generateGetBoundingClientRect = (x = 0, y = 0) => new DOMRect(x, y, 0, 0);
-const virtualElement = {
-  getBoundingClientRect: () => generateGetBoundingClientRect(pos, 0)
-};
-
-const popper = createPopper(
-  virtualElement,
-  menu,
-  {
-    placement: 'bottom-start',
-    modifiers: [{ name: 'eventListeners', options: { scroll: false } }]
-  }
-);
-
-const selection = window.getSelection();
-
-bookContent?.addEventListener('mouseup', event => {
-  const ranges = selection.getAllRange();
-  const range = ranges[ranges.length - 1];
-
-  if (!range.collapsed && range.toString() !== '') {
-    const { x, y } = event;
-    // flew in
-    virtualElement.getBoundingClientRect = () => generateGetBoundingClientRect(x, y);
-  }
-
-  popper.forceUpdate();
-}, false);
-
-document.addEventListener('mousedown', _ => {
-  // eject
-  virtualElement.getBoundingClientRect = () => generateGetBoundingClientRect(pos, 0);
-  popper.forceUpdate();
-}, false);
-
-['mouseover', 'mouseout'].forEach(event => {
-  Array.from(menu.children).forEach(el => {
-    el.addEventListener(event, () => toggleClass(el, 'context_menu_item_active') , false);
-  });
-});
 /**
  *
  * @param {string} selector
- * @returns {Element | null}
+ * @param {{ name: string; click: (e: Event) => void }[]} items
+ * @returns {Element}
  */
-function getElement (selector) {
-  return document.querySelector(selector);
-}
+export default (selector, items) => {
+
+  const fragment = document.createDocumentFragment();
+  const menu = document.createElement('ul');
+  addClass(menu, 'context_menu');
+
+  items.forEach(item => {
+    const li = document.createElement('li');
+    addClass(li, 'context_menu_item');
+    li.innerHTML = `
+      <span class="context_menu_item_icon">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-${item.name}"></use>
+        </svg>
+      </span>
+      <span class="context_menu_item_content">${item.name}</span>
+    `;
+
+    li.addEventListener('click', item.click,);
+
+    ['mouseover', 'mouseout'].forEach(event => {
+      li.addEventListener(event, () => toggleClass(li, 'context_menu_item_active'));
+    });
+
+    menu.appendChild(li);
+  });
+
+  fragment.append(menu);
+
+  const bookContent = getElement(selector);
+
+  bookContent.appendChild(fragment);
+
+  const pos = 0; // init position
+
+  const generateGetBoundingClientRect = (x = 0, y = 0) => new DOMRect(x, y, 0, 0);
+  const virtualElement = {
+    getBoundingClientRect: () => generateGetBoundingClientRect(pos, 0)
+  };
+
+  const popper = createPopper(
+    virtualElement,
+    menu,
+    {
+      placement: 'bottom-start',
+      modifiers: [{ name: 'eventListeners', options: { scroll: false } }]
+    }
+  );
+
+  const selection = window.getSelection();
+
+  bookContent?.addEventListener('mouseup', event => {
+    const ranges = selection.getAllRange();
+    const range = ranges[ranges.length - 1];
+
+    if (!range.collapsed && range.toString() !== '') {
+      const { x, y } = event;
+      // flew in
+      virtualElement.getBoundingClientRect = () => generateGetBoundingClientRect(x, y);
+    }
+
+    popper.forceUpdate();
+  });
+
+  document.body.addEventListener('mousedown', _ => {
+    if (!selection.rangeCount && selection.isCollapsed) {
+      // // prevent click selection flew in
+      // selection.removeAllRanges();
+      // eject
+      virtualElement.getBoundingClientRect = () => generateGetBoundingClientRect(pos, 0);
+      popper.forceUpdate();
+    }
+  });
+
+  /**
+   *
+   * @param {string} selector
+   * @returns {Element | null}
+   */
+  function getElement (selector) {
+    return document.querySelector(selector);
+  }
+
+  return menu;
+};
