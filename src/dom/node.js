c15580004869e8afeeab3515eaa063a0a593b9ae
Aery3c <294725131@qq.com>
2022-05-18 23:22:04 +0800

    fix: 为dom工具添加 getNodeLength 方法

--- src/dom/node.js
+++ src/dom/node.js
@@ -23,3 +23,27 @@ export function getNodeIndex (node) {
 export function isAncestorOf (ancestor, descendant) {
   return !!(ancestor.compareDocumentPosition(descendant) & 16);
 }
+
+/**
+ * 返回节点的长度
+ *
+ * 如果是元素节点, 返回子节点的数量
+ *
+ * 如果是文本节点, 返回节点的文本长度
+ * @param {Node | Text} node
+ * @return {number}
+ */
+export function getNodeLength (node) {
+  switch (node.nodeType) {
+    case Node.ELEMENT_NODE:
+      return node.childNodes.length;
+
+    case Node.CDATA_SECTION_NODE:
+    case Node.COMMENT_NODE:
+    case Node.TEXT_NODE:
+      return node.length;
+
+    default:
+      return 0;
+  }
+}
