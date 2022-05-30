4c55a06d4b992e29dcb1070bb0c1299284e9f819
Aery3c <294725131@qq.com>
2022-05-30 16:45:19 +0800

    feat: 为characterRange添加一些常用方法

--- src/characterRange/index.js
+++ src/characterRange/index.js
@@ -1,3 +1,6 @@
+/**
+ * 在容器上下文中表示一段字符的范围
+ */
 'use strict'
 
 class CharacterRange {
@@ -11,6 +14,79 @@ class CharacterRange {
     this.start = start;
     this.end = end;
     this.containerElement = containerElement;
+  }
+
+  /**
+   * 返回两段范围产生交集的部分, 返回一个新的范围
+   * @param {CharacterRange} otherCharRange
+   * @return {CharacterRange}
+   */
+  intersection (otherCharRange) {
+    return new CharacterRange(Math.max(this.start, otherCharRange.start), Math.min(this.end, otherCharRange.end));
+  }
+
+  /**
+   * 如果range产生交集, 返回true, 否则false
+   * @param {CharacterRange} otherCharRange
+   * @return {boolean}
+   */
+  isIntersects (otherCharRange) {
+    return this.start < otherCharRange.end && this.end > otherCharRange.start;
+  }
+
+  /**
+   * range是否是相邻的
+   * @param {CharacterRange} otherCharRange
+   * @return {boolean}
+   */
+  isAdjoin (otherCharRange) {
+    return this.start === otherCharRange.end || this.end === otherCharRange.start
+  }
+
+  /**
+   * 两个range是否完全重叠在一起
+   * @param otherCharRange
+   * @return {boolean}
+   */
+  isOverlap (otherCharRange) {
+    return this.start === otherCharRange.start && this.end === otherCharRange.end;
+  }
+
+  /**
+   * 并集, 返回一个新的range
+   * @param {CharacterRange} otherCharRange
+   * @return {CharacterRange}
+   *
+   */
+  union (otherCharRange) {
+    return new CharacterRange(Math.min(this.start, otherCharRange.start), Math.max(this.end, otherCharRange.end));
+  }
+
+  /**
+   * 补集, 返回一组新的range
+   * @param {CharacterRange} subCharRange
+   * @return {CharacterRange[]}
+   */
+  complementarySet (subCharRange) {
+    const charSet = [];
+
+    if (this.start < subCharRange.start) {
+      charSet.push(new CharacterRange(this.start, subCharRange.start));
+    }
+
+    if (this.end > subCharRange.end) {
+      charSet.push(new CharacterRange(subCharRange.end, this.end));
+    }
+
+    return charSet;
+  }
+
+  setStart (offset) {
+    this.start = offset;
+  }
+
+  setEnd (offset) {
+    this.end = offset;
   }
 }
 
