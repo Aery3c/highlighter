a7f7c464dc1b20c2aabc18970a4b11afcd60f79c
Aery <294725131@qq.com>
2022-07-01 17:58:47 +0800

    Update characterRange.js

--- template/characterRange/characterRange.js
+++ template/characterRange/characterRange.js
@@ -2,14 +2,14 @@ import { createCharacterRange, utils } from '@/index';
 
 
 const characterRange = createCharacterRange(4, 20);
-utils.highlightACharacterRange(characterRange);
+utils.highlightACharacterRange(characterRange, { className: 'pink' });
 
-const c2 = createCharacterRange(19, 25);
+const c2 = createCharacterRange(15, 18);
 
 setTimeout(function () {
   utils.highlightACharacterRange(c2);
   setTimeout(function () {
-    utils.unhighlightACharacterRange(createCharacterRange(5, 23));
+    utils.unhighlightACharacterRange(createCharacterRange(15, 18), { className: 'pink' });
 
   }, 2000);
 }, 2000);
