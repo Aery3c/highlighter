e1f3d63f99909107ca1860ce57c0a0e3114fdee5
Aery <294725131@qq.com>
2022-07-01 17:59:46 +0800

    Update highlighter.js

--- template/highlighter/highlighter.js
+++ template/highlighter/highlighter.js
@@ -1,36 +1,16 @@
 import { dom, createHighlighter } from '@/index';
 
+const highlighter = createHighlighter('pink');
+const highlighter2 = createHighlighter('yellow');
 
-//
-//
-// const pink = createHighlighter('pink', {
-//   elProps: {
-//     onclick: function () {
-//       console.log(this);
-//     },
-//   }
-// });
-// const yellow = createHighlighter('yellow', {
-//   elProps: {
-//     onclick: function () {
-//       console.log(this);
-//     },
-//   }
-// });
-//
-// dom.gE('#pink').addEventListener('click', function () {
-//   pink.highlightSelection();
-// });
-//
-// dom.gE('#unPink').addEventListener('click', function () {
-//   pink.unhighlightSelection();
-// });
-//
-// dom.gE('#yellow').addEventListener('click', function () {
-//   yellow.highlightSelection();
-// });
-//
-// dom.gE('#unYellow').addEventListener('click', function () {
-//   yellow.unhighlightSelection();
-// });
-//
+dom.gE('#pink').addEventListener('click', function () {
+  highlighter.highlightSelection();
+});
+
+dom.gE('#unPink').addEventListener('click', function () {
+  highlighter.unhighlightSelection();
+});
+
+dom.gE('#yellow').addEventListener('click', function () {
+  highlighter2.highlightSelection();
+});
