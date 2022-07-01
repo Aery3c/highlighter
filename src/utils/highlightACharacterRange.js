a4d5f82087b8db753d3c9b8c717f734c5d8997f3
Aery <294725131@qq.com>
2022-07-01 17:59:10 +0800

    Update highlightACharacterRange.js

--- src/utils/highlightACharacterRange.js
+++ src/utils/highlightACharacterRange.js
@@ -7,13 +7,8 @@ import core from '@/core';
  * @param {Object} [options]
  */
 function highlightACharacterRange (characterRange, options = {}) {
-  const {
-    tagName = core.TAG_NAME,
-    className = core.DEFAULT_CLASS_NAME,
-    containerElement = core.CONTEXT,
-    elAttrs = {},
-    elProps = {}
-  } = options;
+  options = core.utils.createHighlightOptions(options);
+  const { containerElement, tagName, className, elAttrs, elProps } = options;
   const range = characterRange.toRange(containerElement);
   core.utils.appliesToRange(range, tagName, className, elAttrs, elProps);
 }
