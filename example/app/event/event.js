45fb8d9eaaf9312159d4601634ceb4561e144fef
Aery3c <294725131@qq.com>
2022-07-06 17:40:16 +0800

    style: remove all demo

--- example/app/event/event.js
+++
@@ -1,7 +0,0 @@
-/**
- * create custom events
- * @see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
- */
-
-export const UPDATE_MARKS = 'UPDATE_MARKS';
-export const ADD_ANNOTATION = 'ADD_ANNOTATION';
--- example/app/pen.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import { createHighlighter } from '@/index';
-
-export const highlighterPink = createHighlighter('pink', {
-  containerElement: document.querySelector('.book_container')
-});
-
