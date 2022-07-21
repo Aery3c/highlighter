374e226f0ff4da67ebe44c6f71b5df9fb0b4a5ec
Aery <294725131@qq.com>
2022-07-21 19:22:08 +0800

    Update constant.js

--- __tests__/constant.js
+++ __tests__/constant.js
@@ -1,7 +1,9 @@
+'use strict';
+
 export const HTML_TEST_TEMPLATE = `
   <div id="container">  
     <p id="p">
-      <b>Use the buttons below</b> 
+      <b id="b">Use the buttons below</b> 
       to select or deselect the contents of this paragraph.
     </p>
     <button id="select-button">Select paragraph</button>
