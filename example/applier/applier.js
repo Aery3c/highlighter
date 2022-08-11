3263b615b1f34b46e53a93a690708e541f464f71
Aery3c <294725131@qq.com>
2022-08-12 01:02:38 +0800

    feat: test applier

--- example/applier/applier.js
+++ example/applier/applier.js
@@ -1,13 +1,28 @@
 import '../init.scss';
 import '../layout.scss';
 import Applier from '@/applier';
+import { gE } from '@/dom';
 
-const applier = new Applier({ className: 'highlight' });
+const applier = new Applier({ className: 'highlight', elAttrs: { style: 'background-color: yellow;' } });
+const bold = new Applier({ className: 'bold', elAttrs: { style: 'background-color: red; font-weight: bold;' } });
+const toggle = new Applier({ className: 'pink', elAttrs: { style: 'background-color: pink;' } })
 
-document.querySelector('#highlightToRange').addEventListener('click', function () {
+gE('#highlight').addEventListener('click', () => {
   applier.highlightToRange(window.getSelection().getRangeAt(0));
 });
 
-document.querySelector('#unhighlightToRange').addEventListener('click', function () {
+gE('#unhighlight').addEventListener('click', () => {
   applier.unhighlightToRange(window.getSelection().getRangeAt(0));
+});
+
+gE('#bold').addEventListener('click', () => {
+  bold.highlightToRange(window.getSelection().getRangeAt(0));
+});
+
+gE('#unbold').addEventListener('click', () => {
+  bold.unhighlightToRange(window.getSelection().getRangeAt(0));
+});
+
+gE('#toggle').addEventListener('click', () => {
+
 });
