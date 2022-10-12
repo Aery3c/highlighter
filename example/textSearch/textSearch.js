443634d90a03c8d62c066d3124f6a4684a8a7734
Aery3c <294725131@qq.com>
2022-10-12 15:36:54 +0800

    feat: optimize TextSearch Object

--- example/textSearch/textSearch.js
+++ example/textSearch/textSearch.js
@@ -1,5 +1,11 @@
 import TextSearch from '@/highlighter/textSearch';
+// import { wordList } from './random-words';
 
 const textSearch = new TextSearch();
-textSearch.setup('hello');
-console.log(textSearch);
+const content = document.body.textContent;
+console.time('test wordList');
+textSearch.setup(['math', 'Nash Equilibrium']);
+const positions = textSearch.find(content, false);
+console.log(positions);
+console.timeEnd('test wordList');
+console.log(textSearch.trie);
