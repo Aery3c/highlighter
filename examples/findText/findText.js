9790b6e6ee6a4f3ac1ed94f6517199ece834e880
Aery <294725131@qq.com>
2023-03-06 15:15:53 +0800

    Update findText.js

--- examples/findText/findText.js
+++ examples/findText/findText.js
@@ -1,14 +1,74 @@
 // @flow
 import TextSearch from './TextSearch';
+import { CharacterRange, Refills } from '../../src';
+import Highlight from '../../src/utils/highlight';
 
-const textSearch = new TextSearch();
+const textSearch = new TextSearch(),
+  refillsYellow = new Refills({ className: 'yellow' }),
+  refillsOrange = new Refills({ className: 'orange' });
 
-const el = document.querySelector('#search');
-el?.addEventListener('keyup', debounce(() => {
-  // $FlowIgnore
-  console.log(el.value);
-}, 1000));
+function getCharacterRanges (text: string, referenceNode: Element): CharacterRange[] {
+  textSearch.setup(text);
+  return textSearch.find(referenceNode.textContent).filter(pos => pos.string === text).map(pos => {
+    return new CharacterRange(pos.start, pos.end, referenceNode);
+  });
+}
 
+function* generator (text: string, referenceNode: Element) {
+  let count = 0, results = getCharacterRanges(text, referenceNode);
+  while (results.length) {
+    yield results[count++];
+    if (count === results.length) {
+      count = 0;
+    }
+  }
+}
+
+let characterRangeStore, highlights = [], next = null;
+const el = document.querySelector('#search'),
+  referenceNode = document.body;
+
+el.addEventListener('input', debounce(function (e) {
+  let highlight;
+  while ((highlight = highlights.pop())) {
+    highlight.off();
+  }
+
+  if (next) {
+    next.off();
+    next = null;
+  }
+
+  const value = e.target.value;
+  if (value) {
+    characterRangeStore = generator(value, referenceNode);
+    const results = getCharacterRanges(value, referenceNode);
+    results.forEach((cr, indxex) => {
+      highlight = new Highlight(cr, refillsYellow);
+      highlights.push(highlight);
+      highlight.on();
+      if (indxex === 0) {
+        highlight.scrollIntoView({ scrollMode: 'if-needed', block: 'center' });
+      }
+    });
+  }
+
+}, 200));
+
+el.addEventListener('keyup', debounce(function (e) {
+  if (e.keyCode === 13) {
+    if (next) {
+      next.off();
+    }
+
+    const value = e.target.value;
+    if (value) {
+      next = new Highlight(characterRangeStore.next().value, refillsOrange);
+      next.on();
+      next.scrollIntoView({ scrollMode: 'if-needed', block: 'center' })
+    }
+  }
+}, 100));
 
 function debounce (func: Function, wait?: number = 100) {
   let timestamp, args, timeout, context;
