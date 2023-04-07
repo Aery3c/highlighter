19466623dc27da7b4e7301722ba17546f3836376
Aery <294725131@qq.com>
2023-04-07 14:54:21 +0800

    Add files via upload

---
+++ examples/highlighter/highlighter.js
@@ -0,0 +1,37 @@
+// @flow
+import { Highlighter } from '../../src';
+
+const highlighter = new Highlighter({ className: 'yellow', normalize: false });
+
+const toggleColorEl = document.getElementById('toggleColor');
+const useSelectionEl = document.getElementById('useSelection');
+const unSelectionEl = document.getElementById('unSelection');
+
+toggleColorEl?.addEventListener('click', function () {
+  if (highlighter.options.className === 'yellow') {
+    highlighter.setOptions({ className: 'blue' })
+  } else {
+    highlighter.setOptions({ className: 'yellow' });
+  }
+});
+
+useSelectionEl?.addEventListener('click', function () {
+  highlighter.useSelection();
+  console.log(highlighter.highlights);
+});
+
+unSelectionEl?.addEventListener('click', function () {
+  highlighter.unSelection();
+  console.log(highlighter.highlights);
+});
+
+highlighter.on('click', function (h) {
+  if(window.confirm('remove?')) {
+    highlighter.removeHighlight(h);
+    console.log(highlighter.highlights);
+  }
+})
+
+highlighter.deserialize([{ start: 50, end: 55, className: 'yellow' }, { start: 53, end: 59, className: 'orange' }, { start: 100, end: 105, className: 'orange' }, { start: 190, end: 205, className: 'blue' }]);
+
+
